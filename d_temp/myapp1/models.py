ea13921615b600a42da370869fdeaefea4ada332
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-06-01 23:59:59 +0530

    admin

--- d_temp/myapp1/models.py
+++ d_temp/myapp1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class reg(models.Model):
+    name=models.CharField(max_length=40)
+    email=models.CharField(max_length=30)
+    password=models.CharField(max_length=15)
+    contact=models.CharField(max_length=40)
