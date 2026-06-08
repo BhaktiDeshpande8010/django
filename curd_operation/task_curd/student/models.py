6470a9435524fdd478c7dc5693481b620c884774
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-06-08 14:41:56 +0530

    CURD operation

---
+++ curd_operation/task_curd/student/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+
+class reg(models.Model):
+    name=models.CharField(max_length=40)
+    email=models.CharField(max_length=30)
+    password=models.CharField(max_length=15)
+    contact=models.CharField(max_length=40)
+    address=models.CharField(max_length=100,default='Pune')
---
+++ curd_operation/task_curd/student/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,HttpResponse
+from .models import reg
+
+def home(request):
+    return render(request,"home.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def registration(request):   # changed name
+    return render(request,"reg.html")
+
+def saveform(request):
+    if request.method=="POST":
+        n=request.POST.get("name")
+        em=request.POST.get("email")
+        ps=request.POST.get("password")
+        cn=request.POST.get("contact")
+        ad=request.POST.get("address")
+
+        r=reg(
+            name=n,
+            email=em,
+            password=ps,
+            contact=cn,
+            address=ad
+        )
+        r.save()
+
+        return HttpResponse("Registration completed successfully")
+
+    else:
+        return HttpResponse("Failed")
